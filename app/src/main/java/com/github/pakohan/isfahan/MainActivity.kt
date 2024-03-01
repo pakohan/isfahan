a2a8f65988a1e8180db1ac50cddde38b89ddb3be
Patrick Kohan <pakohan@users.noreply.github.com>
2024-03-01 16:11:09 +0800

    Aktualisieren von MainActivity.kt

--- app/src/main/java/com/github/pakohan/isfahan/MainActivity.kt
+++ app/src/main/java/com/github/pakohan/isfahan/MainActivity.kt
@@ -4,6 +4,7 @@ import com.github.pakohan.isfahan.R
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
