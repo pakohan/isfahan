5d3a1c1eb8a2ba3f8eeaf0fde0401180383959b9
Patrick Kohan <pakohan@users.noreply.github.com>
2024-03-02 17:32:01 +0800

    Aktualisieren von MainActivity.kt

--- app/src/main/java/com/github/pakohan/isfahan/MainActivity.kt
+++ app/src/main/java/com/github/pakohan/isfahan/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         setContent {
             Column {
                 Text(text =  myText)
-                Button() { Text("Click!") }
+                Button(onClick = {}) { Text("Click!") }
             }
         }
     }
