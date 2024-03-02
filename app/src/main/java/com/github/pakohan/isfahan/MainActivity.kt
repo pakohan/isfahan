51440ea9fb4a6b643247a55221784b5ca9636d28
Patrick Kohan <pakohan@users.noreply.github.com>
2024-03-02 17:51:34 +0800

    Aktualisieren von MainActivity.kt

--- app/src/main/java/com/github/pakohan/isfahan/MainActivity.kt
+++ app/src/main/java/com/github/pakohan/isfahan/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import com.github.pakohan.isfahan.R
 
