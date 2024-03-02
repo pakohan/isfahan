e83b55d72388e46f31293599349e0512f114f29b
Patrick Kohan <pakohan@users.noreply.github.com>
2024-03-02 18:37:19 +0800

    Aktualisieren von MainActivity.kt

--- app/src/main/java/com/github/pakohan/isfahan/MainActivity.kt
+++ app/src/main/java/com/github/pakohan/isfahan/MainActivity.kt
@@ -3,25 +3,14 @@ package com.github.pakohan.isfahan.app
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
 import com.github.pakohan.isfahan.R
 
 class MainActivity : AppCompatActivity() {
-    val text = mutableStateOf(getString(R.string.hello))
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var myText by text
         setContent {
-            Column {
-                Text(text =  myText)
-                Button(onClick = { myText = "clicked" }) { Text("Click!") }
-            }
+            Text(text = getString(R.string.hello))
         }
     }
 }
