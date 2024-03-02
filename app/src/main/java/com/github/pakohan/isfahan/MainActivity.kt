904905c466ac488dade3577012777269b347766f
Patrick Kohan <pakohan@users.noreply.github.com>
2024-03-02 18:46:15 +0800

    Aktualisieren von MainActivity.kt

--- app/src/main/java/com/github/pakohan/isfahan/MainActivity.kt
+++ app/src/main/java/com/github/pakohan/isfahan/MainActivity.kt
@@ -5,6 +5,12 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.Text
 import com.github.pakohan.isfahan.R
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
