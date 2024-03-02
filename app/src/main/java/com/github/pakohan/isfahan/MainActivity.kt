d757bea450856878502ec08ed59f6f07b813704d
Patrick Kohan <pakohan@users.noreply.github.com>
2024-03-02 11:45:01 +0800

    Aktualisieren von MainActivity.kt

--- app/src/main/java/com/github/pakohan/isfahan/MainActivity.kt
+++ app/src/main/java/com/github/pakohan/isfahan/MainActivity.kt
@@ -2,11 +2,14 @@ package com.github.pakohan.isfahan.app
 
 import com.github.pakohan.isfahan.R
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Text
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent { Text(getString(R.string.hello)) }
     }
 }
